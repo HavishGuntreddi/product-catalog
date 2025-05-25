53a3c319e1cb310aeede157c84d0d5e33368e8e4
Havish Guntreddi <mailtohavish@gmail.com>
2025-05-25 19:56:33 -0400

    dev vs prod fix

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,12 @@ app.use(express.json());
 
 app.use("/api/products", productRoute);
 
+
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/dist")))
+  app.use(express.static(path.join(__dirname, "..", "/client/dist")))
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname,  ".." ,"client", "dist", "index.html"));
   })
 }
 
